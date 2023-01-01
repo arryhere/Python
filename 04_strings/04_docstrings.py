89faf081e45b419c95f503101c1fce5b4cc9229c
Arijit <arijitdasofficial@gmail.com>
2023-01-02 02:27:14 +0530

    feat: doc strings

---
+++ 04_strings/04_docstrings.py
@@ -0,0 +1,13 @@
+'''
+• Docstrings in python
+• Python docstrings are string literals that appear right after the definition of a function, method, class or module
+'''
+
+
+def square(num):
+  '''Function that returns the square of a given number'''
+  return num**2
+
+
+print(square(5))        # 25
+print(square.__doc__)   # Function that returns the square of a given number
