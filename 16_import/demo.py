87b4066e6a6606d2487fb1d353baecb57a3c73a0
Arijit <arijitdasofficial@gmail.com>
2023-03-07 19:26:47 +0530

    feat: import

--- 16_import/demo.py
+++
@@ -1,9 +0,0 @@
-
-def greet():
-  print('Hello, have a nice day!')
-
-
-config = {
-    "name": "Arijit Das",
-    "age": 22
-}
---
+++ 16_import/export.py
@@ -0,0 +1,14 @@
+config = {
+    "name": "Arijit Das",
+    "age": 22
+}
+
+
+def greet():
+  print('Hello, have a nice day!')
+
+
+print("__name__:", __name__)  # __main__
+
+if (__name__ == "__main__"):
+  greet()
