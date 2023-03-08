7d3a25760f5cbf0d5332550aa8d9fbdf99374fb7
Arijit <arijitdasofficial@gmail.com>
2023-03-08 18:50:29 +0530

    feat: local vs global variables

---
+++ 02_variables-datatypes/04_local-vs-global-variables.py
@@ -0,0 +1,66 @@
+'''
+• local variable: variable that is defined within a function and is only accessible within that function. It is created when the
+  function is called and is destroyed when the function returns
+• global variable: variable that is defined outside of a function and is accessible anywhere in your code file
+'''
+
+
+x = 5  # global variable
+
+
+def demo():
+  x = 4  # local variable
+  print(f"The value of x is: {x}")  # The value of x is: 4
+
+
+demo()
+
+print(f"The value of x is: {x}")    # The value of x is: 5
+
+
+print('-------------------------------------------------------------------------------------------------------------------------')
+
+'''
+• global keyword
+• global keyword is used to declare that a variable is a global variable and should be accesed from global scope.
+'''
+
+
+y = 5  # global variable
+
+
+def demo():
+  global y
+  y = 4  # local variable
+  print(f"The value of y is: {y}")  # The value of y is: 4
+
+
+demo()
+
+print(f"The value of y is: {y}")    # The value of y is: 4
+
+
+print('-------------------------------------------------------------------------------------------------------------------------')
+
+
+'''
+• locals()
+• return a dictionary containing the current scope's local variables
+'''
+
+print(__name__)         # __main__
+print(__doc__)
+'''
+• local variable: variable that is defined within a function and is only accessible within that function. It is created when the
+  function is called and is destroyed when the function returns
+• global variable: variable that is defined outside of a function and is accessible anywhere in your code file
+'''
+print(__file__)         # d:\Study\Python\02_variables-datatypes\04_local-vs-global-variables.py
+print(__package__)      # None
+print(__loader__)       # <_frozen_importlib_external.SourceFileLoader object at 0x0000018F47682F50>
+print(__spec__)         # None
+print(__annotations__)  # {}
+print(__builtins__)     # <module 'builtins' (built-in)>
+
+
+print('-------------------------------------------------------------------------------------------------------------------------')
