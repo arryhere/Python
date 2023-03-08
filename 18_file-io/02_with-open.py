81b9f9fcc5cf8670565b982233c6043dc5dfcf0b
Arijit <arijitdasofficial@gmail.com>
2023-03-09 03:27:41 +0530

    feat: file methods

--- 18_file-io/02_with-open.py
+++ 18_file-io/02_with-open.py
@@ -2,6 +2,10 @@ import os
 
 os.chdir(os.path.join(os.getcwd(), '18_file-io'))   # D:\Study\Python\18_file-io
 
+
+print('-------------------------------------------------------------------------------------------------------------------------')
+
+
 with open('readFile.txt', 'r', encoding="utf-8") as file:
   print(file.read())
   '''
