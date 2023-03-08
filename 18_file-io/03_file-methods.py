d642475913738c16b7a0e4ff5e784edd752a0d90
Arijit <arijitdasofficial@gmail.com>
2023-03-09 03:22:47 +0530

    feat: file methods

--- 18_file-io/03_file-methods.py
+++ 18_file-io/03_file-methods.py
@@ -6,6 +6,8 @@
 • readlines()
 • write()
 • writelines()
+• seek()
+• tell()
 '''
 
 import os
@@ -96,3 +98,68 @@ with open('./writeFile.txt', 'w', encoding="utf-8") as f:
 
 
 print('-------------------------------------------------------------------------------------------------------------------------')
+
+
+'''
+• seek()
+• takes 2 args: offset, whence
+• whence value of 0: beginning of the file (default), 1: current file position, 2: end of the file as the reference point
+• position is computed from adding offset to a reference point
+• If the file is opened in text mode (without b), only offsets returned by tell() are legal
+  Use of other offsets causes undefined behavior
+'''
+with open('./readFile.txt', 'r', encoding="utf-8") as f:
+  print(f.read(1))      # H
+  print(f.read(1))      # e
+  f.seek(0)             # Go to the 2nd byte in the file
+  print(f.read(1))      # H
+  f.seek(2)             # Go to the 2nd byte in the file
+  print(f.read(1))      # l
+
+with open('./readFile.txt', 'rb') as f:
+  f.seek(-3, 2)         # Go to the 3rd byte brfore end in the file
+  print(f.read(1))      # b'n'
+
+with open('./readFile.txt', 'rb') as f:
+  print(f.read(1))      # b'H'
+  print(f.read(1))      # b'e'
+  f.seek(4, 1)
+  print(f.read(1))      # b'W'
+
+
+print('-------------------------------------------------------------------------------------------------------------------------')
+
+
+'''
+• tell()
+•  returns an integer giving the file object’s current position in the file
+• represented as number of bytes from the beginning of the file when in binary mode and an opaque number when in text mode
+'''
+with open('./readFile.txt', 'r', encoding="utf-8") as f:
+  print(f.tell())   # 0
+  f.read(1)         # H
+  f.read(1)         # e
+  f.read(1)         # l
+  print(f.tell())   # 3
+
+
+print('-------------------------------------------------------------------------------------------------------------------------')
+
+'''
+• truncate()
+• truncate the file to specific bytes
+'''
+with open('./writeFile.txt', 'r+', encoding="utf-8") as f:
+  f.read()
+  count = f.truncate(2)
+  print(count)  # 2
+
+
+print('-------------------------------------------------------------------------------------------------------------------------')
+
+
+'''
+• References
+• https://stackoverflow.com/questions/21533391/seeking-from-end-of-file-throwing-unsupported-exception
+• 
+'''
